844fe11d57e744c185880055550286cb1ca26485
Pampam <piankovpe@gmail.com>
2023-03-12 23:34:50 +0600

    add description

--- routers/delete.py
+++ routers/delete.py
@@ -12,9 +12,12 @@ delete_router = APIRouter(prefix="/delete",
 @delete_router.post("/expired",
                     name="Delete all expired records from Db",
                     response_model=DeleteExpiredResponse,
+                    description="Deletes all expired records from given Table in Db",
+                    response_description="Correct Json response with cleared Table and call time",
                     )
-async def clear_expired(admin_key: str = Header(),
-                        db_model: str = Header("all"),
+async def clear_expired(admin_key: str = Header(description="Required access key"),
+                        db_model: str = Header("all",
+                                               description="Table name"),
                         db: Session = Depends(db_session)
                         ):
     return delete_all_expired_in_table(admin_key=admin_key, db=db, db_model=db_model)
--- routers_functions/delete_functions.py
+++ routers_functions/delete_functions.py
@@ -1,13 +1,14 @@
 import os
+from datetime import datetime
+from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 from database.models import DbKeys, DbCustom, DbRandom
-from fastapi import HTTPException, status
 from schemas.schemas import DeleteExpiredResponse
-from datetime import datetime
 from routers_functions.scope_all import del_expired
 
 
 def delete_all_expired_in_table(admin_key: str, db: Session, db_model: str = "all", ):
+    """Deletes expired records from given table or all tables of provided DB"""
     call_time = datetime.utcnow()
     chosen_table = db_model.lower().replace(" ", "")
     if admin_key == os.getenv("admin_key"):
@@ -26,4 +27,3 @@ def delete_all_expired_in_table(admin_key: str, db: Session, db_model: str = "al
             return DeleteExpiredResponse(table=chosen_table.__name__, call_time=call_time)
     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                         detail="Wrong Admin-key")
-
