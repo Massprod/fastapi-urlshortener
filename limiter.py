01be8a6981afefb925121e750dd13411db62e8e2
Pampam <piankovpe@gmail.com>
2023-03-13 21:43:51 +0600

    add request limiter

---
+++ limiter.py
@@ -0,0 +1,8 @@
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+
+req_limiter = Limiter(key_func=get_remote_address)
+random_limit = "15/minute"
+custom_limit = "15/minute"
+delete_limit = "10/minute"
+register_limit = "1/minute"
