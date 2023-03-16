ad4150706b9f247a7c29aacaea3606f8f690e887
Pampam <piankovpe@gmail.com>
2023-03-16 20:50:27 +0600

    add fixtures custom_route

---
+++ tests/test_custom_route_fixtures.py
@@ -0,0 +1,54 @@
+import pytest
+from pydantic import BaseModel
+from schemas.schemas import CustomShort
+from database.models import DbKeys
+import string
+import random
+# pytest see these fixtures, but I can't use them anywhere before explicitly Import them.
+# How I understand: they should be Session wide once created, and used without imports.
+# Change it if I find why/how.
+
+
+@pytest.fixture(scope="session")
+def base_url():
+    return "http://test/"
+
+
+@pytest.fixture(scope="function")
+def no_key_request():
+    return CustomShort(origin_url="https://github.com/Massprod/FastAPI_UrlShort",
+                       custom_name="nokeyused",
+                       expire_days=5,
+                       )
+
+
+@pytest.fixture(scope="function")
+def with_key_entity():
+    return DbKeys(email="test_custom_with_key@gmail.com",
+                  username="test_custom_with_key",
+                  api_key="test_with",
+                  activation_link="activated_with_key",
+                  link_send=True,
+                  activated=True,
+                  expire_date=None,
+                  )
+
+
+@pytest.fixture(scope="function")
+def with_key_request():
+    return CustomShort(origin_url="https://github.com/Massprod/UdemyFastAPI",
+                       custom_name="keyused",
+                       expire_days=20,
+                       )
+
+
+@pytest.fixture(scope="function")
+def limits_entity_with_key():
+    return DbKeys(email="test_expire_with_key@gmail.com",
+                  username="test_expire_with_key",
+                  api_key="test_expire",
+                  activation_link="expire_key",
+                  link_send=True,
+                  activated=True,
+                  expire_date=None,
+                  )
