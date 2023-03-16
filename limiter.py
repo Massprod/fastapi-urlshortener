33658e0edba7983f3d5ca97226792c08677c0416
Pampam <piankovpe@gmail.com>
2023-03-17 01:54:40 +0600

    change limits

--- limiter.py
+++ limiter.py
@@ -2,7 +2,7 @@ from slowapi import Limiter
 from slowapi.util import get_remote_address
 
 req_limiter = Limiter(key_func=get_remote_address)
-random_limit = "15/minute"
-custom_limit = "15/minute"
-delete_limit = "10/minute"
-register_limit = "10/minute"
+random_limit = "120/minute"
+custom_limit = "150/minute"
+delete_limit = "100/minute"
+register_limit = "85/minute"
