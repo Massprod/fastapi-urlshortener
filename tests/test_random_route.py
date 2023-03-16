f4b75e27860a6a4e6cdad934e21c83f63086ce88
Pampam <piankovpe@gmail.com>
2023-03-16 15:09:24 +0600

    add test for infinite_loop break

--- tests/test_random_route.py
+++ tests/test_random_route.py
@@ -1,8 +1,13 @@
+import httpx
 import pytest
 from httpx import AsyncClient
 from conf_test_db import shorty, override_db_session
 from routers_functions.scope_all import expire_date
 from database.models import DbRandom
+
+max_length = 10
+min_length = 4
+expire_days_target = 7
 
 
 @pytest.mark.asyncio
@@ -12,17 +17,17 @@ async def test_add_new_random():
         db = next(override_db_session())
         response_1 = await client.post("/random/add",
                                        json={"origin_url": "https://github.com/Massprod/FastAPI_UrlShort",
-                                             "short_length": 4}
+                                             "short_length": min_length}
                                        )
         assert response_1.status_code == 200
         created_short_1 = response_1.json()["short_url"]
         db_record_2 = db.query(DbRandom).filter_by(short_url=created_short_1).first()
         assert db_record_2
-        target_expire_date = expire_date(days=7)
+        target_expire_date = expire_date(days=expire_days_target)
         assert target_expire_date.day == db_record_2.expire_date.day
         response_2 = await client.post("/random/add",
                                        json={"origin_url": "https://github.com/Massprod/FastAPI_UrlShort",
-                                             "short_length": 10}
+                                             "short_length": max_length}
                                        )
         assert response_2.status_code == 200
 
@@ -33,12 +38,12 @@ async def test_add_new_random_with_broken_url():
     async with AsyncClient(app=shorty, base_url="http://test") as client:
         response = await client.post("/random/add",
                                      json={"origin_url": "https://www.reddit.com/testetes",
-                                           "short_length": 4}
+                                           "short_length": min_length}
                                      )
         assert response.status_code == 400
         timeout_response = await client.post("/random/add",
                                              json={"origin_url": "https://pikabu.ru/dasd",
-                                                   "short_length": 10}
+                                                   "short_length": max_length}
                                              )
         assert timeout_response.status_code == 400
 
@@ -49,16 +54,39 @@ async def test_add_new_random_with_wrong_length():
     async with AsyncClient(app=shorty, base_url="http://test") as client:
         response_1 = await client.post("/random/add",
                                        json={"origin_url": "https://github.com/Massprod/FastAPI_UrlShort",
-                                             "short_length": 11}
+                                             "short_length": max_length + 1}
                                        )
         assert response_1.status_code == 400
         response_2 = await client.post("/random/add",
                                        json={"origin_url": "https://github.com/Massprod/FastAPI_UrlShort",
-                                             "short_length": 3}
+                                             "short_length": min_length - 1}
                                        )
         assert response_2.status_code == 400
         response_3 = await client.post("/random/add",
                                        json={"origin_url": "https://github.com/Massprod/FastAPI_UrlShort",
-                                             "short_length": -10.5}
+                                             "short_length": min_length * -1}
                                        )
         assert response_3.status_code == 400
+
+
+@pytest.mark.asyncio
+async def test_add_new_random_infinite_limit(mocker):
+    """Test standard response with creating duplicated of short_url, and trying to insert in Db"""
+    async with AsyncClient(app=shorty, base_url="http://test") as client:
+        database = next(override_db_session())
+        short_duplicate = "test_loop_out"
+        mocker.patch("routers_functions.random_url_functions.create_rshort",
+                     return_value=short_duplicate)
+        response = await client.post("/random/add",
+                                     json={"origin_url": "https://github.com/Massprod/FastAPI_UrlShort",
+                                           "short_length": 5}
+                                     )
+        assert response.status_code == 200
+        inserted_short = response.json()["short_url"]
+        exist = database.query(DbRandom).filter_by(short_url=inserted_short).first()
+        assert exist
+        response_2 = await client.post("/random/add",
+                                       json={"origin_url": "https://github.com/Massprod/FastAPI_UrlShort",
+                                             "short_length": 5})
+        assert response_2.status_code == 508
+        assert pytest.raises(httpx.HTTPStatusError)
