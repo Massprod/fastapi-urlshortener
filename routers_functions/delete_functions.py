7af9b59b8c89b767541851b52838cf41fb565679
Pampam <piankovpe@gmail.com>
2023-03-19 18:21:59 +0600

    change name variables

--- routers_functions/delete_functions.py
+++ routers_functions/delete_functions.py
@@ -10,20 +10,20 @@ from routers_functions.scope_all import del_expired
 def delete_all_expired_in_table(admin_key: str, db: Session, db_model: str = "all", ):
     """Deletes expired records from given table or all tables of provided DB"""
     call_time = datetime.utcnow()
-    chosen_table = db_model.lower().replace(" ", "")
+    chosen_model = db_model.lower().replace(" ", "")
     if admin_key == os.getenv("admin_key"):
-        tables = [DbKeys, DbShort]
-        if chosen_table == "all":
-            for _ in tables:
+        models = [DbKeys, DbShort]
+        if chosen_model == "all":
+            for _ in models:
                 del_expired(db_model=_, db=db, delete_all=True)
             return DeleteExpiredResponse(table="all", call_time=call_time)
         try:
-            chosen_table = [_ for _ in tables if _.__name__.lower() == chosen_table][0]
+            chosen_model = [_ for _ in models if _.__name__.lower() == chosen_model][0]
         except IndexError:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail="Table doesn't exist")
-        if chosen_table:
-            del_expired(db_model=chosen_table, db=db, delete_all=True)
-            return DeleteExpiredResponse(table=chosen_table.__name__, call_time=call_time)
+                                detail=f"Table with name: {chosen_model} doesn't exist")
+        if chosen_model:
+            del_expired(db_model=chosen_model, db=db, delete_all=True)
+            return DeleteExpiredResponse(table=chosen_model.__name__, call_time=call_time)
     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                         detail="Wrong Admin-key")
